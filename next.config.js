b5b98e72e2b18feff2a5346d95ab50cb41007122
Harsh Vitra <jainharsh44@gmail.com>
2022-06-08 06:01:16 +0530

    Ignore typescript errors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  env: {
+    MAPBOXGL_ACCESS_TOKEN: process.env.MAPBOXGL_ACCESS_TOKEN,
+  },
+  // your config for other plugins or the general next.js here...
+  devIndicators: {
+    autoPrerender: false,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+};
